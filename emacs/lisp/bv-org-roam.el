f00cb6569a315c7b6dff828ce3a412e471328c04
Ayan Das <bvits@riseup.net>
2024-04-11 00:09:34 +0530

    feat: add lisp file bv-org-roam.el

---
+++ emacs/lisp/bv-org-roam.el
@@ -0,0 +1,126 @@
+;;; bv-org-roam.el --- Emacs configuration file -*- lexical-binding: t -*-
+
+;; Copyright (C) 2024 Ayan Das <bvits@riseup.net>
+
+;; Author: Ayan Das <bvits@riseup.net>
+;; URL: https://github.com/b-vitamins/.config/emacs/lisp/bv-org-roam.el
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "29.3"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; "What are we to do with what we have written down? Certainly at first, we will
+;; produce mostly garbage.  But we have been educated to expect something useful
+;; from our activities, and soon lose confidence if nothing useful seems to
+;; result.  We should therefore reflect on whether and how we arrange our notes so
+;; they are available for later access." - Niklas Luhmann
+
+;;; Code:
+
+(require 'org)
+(require 'org-roam)
+(require 'marginalia)
+
+(defvar bv-org-roam-capture-templates
+  '(
+    ("f" "fleeting" plain "%?"
+     :target (file+head "notes/${slug}-%<%Y%m%d%H%M%S>.org"
+                        "#+TITLE: ${title}\n#+FILETAGS: :fleeting:\n#+SETUPFILE: ~/.config/emacs/setup/setupfile.org\n")
+     :unnarrowed t)
+
+    ("F" "fleeting-timed" plain "%?"
+     :target (file+head "notes/${slug}-%<%Y%m%d%H%M%S>.org"
+                        "#+TITLE: ${title}\n#+FILETAGS: :fleeting:\n#+SETUPFILE: ~/.config/emacs/setup/setupfile.org\n#+BEGIN: clocktable :maxlevel 2 :scope nil :emphasize nil\n#+CAPTION:\n#+END\n")
+     :unnarrowed t)
+
+    ("c" "concept" plain "%?"
+     :target (file+head "notes/${slug}-%<%Y%m%d%H%M%S>.org"
+                        "#+TITLE: ${title}\n#+FILETAGS: :concept:\n#+SETUPFILE: ~/.config/emacs/setup/setupfile.org\n")
+     :unnarrowed t)
+
+    ("C" "concept-timed" plain "%?"
+     :target (file+head "notes/${slug}-%<%Y%m%d%H%M%S>.org"
+                        "#+TITLE: ${title}\n#+FILETAGS: :concept:\n#+SETUPFILE: ~/.config/emacs/setup/setupfile.org\n#+BEGIN: clocktable :maxlevel 2 :scope nil :emphasize nil\n#+CAPTION:\n#+END\n")
+     :unnarrowed t)
+
+    ("l" "literature" plain "%?"
+     :target (file+head "notes/${slug}-%<%Y%m%d%H%M%S>.org"
+                        "#+TITLE: ${title}\n#+FILETAGS: :literature:\n#+SETUPFILE: ~/.config/emacs/setup/setupfile.org\n")
+     :unnarrowed t)
+
+    ("L" "literature-timed" plain "%?"
+     :target (file+head "notes/${slug}-%<%Y%m%d%H%M%S>.org"
+                        "#+TITLE: ${title}\n#+FILETAGS: :literature:\n#+SETUPFILE: ~/.config/emacs/setup/setupfile.org\n#+BEGIN: clocktable :maxlevel 2 :scope nil :emphasize nil\n#+CAPTION:\n#+END\n")
+     :unnarrowed t)
+
+    ("p" "problem" plain "%?"
+     :target (file+head "notes/${slug}-%<%Y%m%d%H%M%S>.org"
+                        "#+TITLE: ${title}\n#+FILETAGS: :problem:\n#+TEXTBOOK: \n#+SETUPFILE: ~/.config/emacs/setup/setupfile.org\n")
+     :unnarrowed t)
+
+    ("P" "problem-timed" plain "%?"
+     :target (file+head "notes/${slug}-%<%Y%m%d%H%M%S>.org"
+                        "#+TITLE: ${title}\n#+FILETAGS: :problem:\n#+TEXTBOOK: \n#+SETUPFILE: ~/.config/emacs/setup/setupfile.org\n#+BEGIN: clocktable :maxlevel 2 :scope nil :emphasize nil\n#+CAPTION:\n#+END\n")
+     :unnarrowed t)
+    ))
+
+(defcustom bv-org-roam-node-display-template
+  (concat
+   (propertize "" 'face 'org-todo)
+   (propertize " ${hierarchy:100} " 'face 'italic)
+   (propertize " ${directories:20} " 'face 'italic)
+   (propertize " ${tags:60} " 'face 'org-tag)
+   (propertize " ${backlinkscount:6} " 'face 'italic))
+  "Template for displaying nodes in Org Roam."
+  :type 'string
+  :group 'bv-org-roam)
+
+(defcustom bv-org-roam-node-annotation-function
+  (lambda (node)
+    (marginalia--time (org-roam-node-file-mtime node)))
+  "Function to annotate Org Roam nodes with their modification time."
+  :type 'function
+  :group 'bv-org-roam)
+
+(cl-defmethod org-roam-node-directories ((node org-roam-node))
+  "Return the uppercase directory of the NODE's file.
+The directory is relative to `org-roam-directory'."
+  (if-let ((dirs (file-name-directory (file-relative-name (org-roam-node-file node) org-roam-directory))))
+      (format "%s" (upcase (car (split-string dirs "/"))))
+    ""))
+
+(cl-defmethod org-roam-node-backlinkscount ((node org-roam-node))
+  "Return the count of backlinks to the NODE as a string."
+  (let* ((count (caar (org-roam-db-query
+                       [:select (funcall count source)
+                                :from links
+                                :where (= dest $s1)
+                                :and (= type "id")]
+                       (org-roam-node-id node)))))
+    (format "[%d]" count)))
+
+(cl-defmethod org-roam-node-hierarchy ((node org-roam-node))
+  "Return the hierarchy string for the NODE."
+  (let ((level (org-roam-node-level node)))
+    (concat
+     (when (> level 0) (concat (org-roam-node-file-title node) ">"))
+     (when (> level 1) (concat (string-join (org-roam-node-olp node) ">") ">"))
+     (org-roam-node-title node))))
+
+(provide 'bv-org-roam)
+;;; bv-org-roam.el ends here
