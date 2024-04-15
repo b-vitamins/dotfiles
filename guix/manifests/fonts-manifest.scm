dde0e43fb530844f5178ec658c36547ee3816df1
Ayan Das <bvits@riseup.net>
2024-04-15 23:06:30 +0530

    add fonts-manifest.scm

---
+++ guix/manifests/fonts-manifest.scm
@@ -0,0 +1,16 @@
+(use-modules (gnu packages fonts))
+
+(specifications->manifest '(
+"font-gnu-unifont"
+"font-gnu-freefont"
+"font-google-noto"
+"font-google-noto-serif-cjk"
+"font-google-noto-sans-cjk"
+"font-google-noto"
+"font-liberation"
+"font-hack"
+"font-fira-go"
+"font-fira-mono"
+"font-fira-code"
+"font-fira-sans"
+))
