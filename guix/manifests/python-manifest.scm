40e4c98a12fd78e684acee60cff17af8055632fe
Ayan Das <bvits@riseup.net>
2024-04-15 23:07:34 +0530

    update python-manifest.scm

--- guix/manifests/python-manifest.scm
+++ guix/manifests/python-manifest.scm
@@ -1,22 +1,19 @@
-(use-modules (gnu packages python))
+(use-modules (gnu packages python)
+             (myguix packages python-pqrs))
 
-;; Define the Python package manifest with a selection of commonly used modules
-(specifications->manifest '("python@3.10.7" ;Core Python language, specific version
-                            "python-numpy@1.23.2" ;Numerical computing with Python
-                            "python-matplotlib@3.8.2" ;Plotting library for Python
-                            "python-scipy@1.12.0" ;Scientific computing tools for Python
-                            "python-jedi@0.18.2" ;Autocompletion library for Python
+
+(specifications->manifest '("python" ;Core Python language, specific version
+                            "python-numpy" ;Numerical computing with Python
+                            "python-matplotlib" ;Plotting library for Python
+                            "python-scipy" ;Scientific computing tools for Python
+                            "python-jedi" ;Autocompletion library for Python
                             "python-jedi-language-server" ;Language server based on Jedi for IDE-like features
-                            "python-flake8@4.0.1" ;Static code analysis tool for Python
-                            "python-black@22.3.0" ;The uncompromising Python code formatter
+                            "python-flake8" ;Static code analysis tool for Python
+                            "python-black" ;The uncompromising Python code formatter
                             "python-pandas" ;Data structures and data analysis tools
                             "python-requests" ;Elegant and simple HTTP library for Python
                             "python-sqlalchemy" ;SQL toolkit and Object-Relational Mapping (ORM) for Python
                             "python-pytest" ;Testing framework for Python
                             "python-flask" ;A lightweight WSGI web application framework
-                            "python-boto3" ;Amazon Web Services (AWS) SDK for Python
                             "python-scikit-learn" ;Machine learning in Python
-                            "python-virtualenv" ;Tool to create isolated Python environments
-                            "python-ipython" ;Enhanced interactive Python shell
-                            "python-notebook" ;Web-based notebook environment for interactive computing
                             ))
