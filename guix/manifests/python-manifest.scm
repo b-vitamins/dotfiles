8251caf00d033ac9c7570955f4bed2cc6fbc3831
Ayan Das <bvits@riseup.net>
2024-04-17 02:41:30 +0530

    feat: add more packages

--- guix/manifests/python-manifest.scm
+++ guix/manifests/python-manifest.scm
@@ -1,6 +1,10 @@
+;; -*- mode: scheme; -*-
+;; python-manifest.scm
 (use-modules (gnu packages python)
+             (gnu packages python-xyz)
+             (gnu packages python-web)
+             (gnu packages machine-learning)
              (myguix packages python-pqrs))
-
 
 (specifications->manifest '("python" ;Core Python language, specific version
                             "python-numpy" ;Numerical computing with Python
@@ -16,4 +20,13 @@
                             "python-pytest" ;Testing framework for Python
                             "python-flask" ;A lightweight WSGI web application framework
                             "python-scikit-learn" ;Machine learning in Python
+                            "python-pytorch" ;Tensors and Dynamic neural networks in Python with strong GPU acceleration
+                            "python-aiohttp" ;Async HTTP client/server framework
+                            "python-httpx" ;A fully featured HTTP client for Python 3, which provides sync and async APIs
+                            "python-websockets" ;A library for building WebSocket servers and clients in Python with a focus on correctness and simplicity
+                            "python-rich" ;Rich text and beautiful formatting in the terminal
+                            "python-django" ;The Web framework for perfectionists with deadlines
+                            "python-grobid-client-python" ;Simple Python client for GROBID REST services for extracting, parsing, and annotating academic documents
+                            "python-pyalex" ;Python interface to the OpenAlex database, providing easy data access for bibliographic and citation information
                             ))
+
