1ae086e9c066d6ea263a3033a3a80c26a5b7fa5d
Ayan Das <bvits@riseup.net>
2024-04-17 02:41:15 +0530

    apply guix style

--- guix/manifests/texlive-manifest.scm
+++ guix/manifests/texlive-manifest.scm
@@ -1,5 +1,11 @@
+;; -*- mode: scheme; -*-
+;; texlive-manifest.scm
 (use-modules (gnu packages texlive))
 
-;; Define the Emacs package manifest
-(specifications->manifest '("texlive" "texlive-biblatex" "texlive-biber"
-                            "texlive-tex-gyre" "texlive-tex-gyre-math"))
+(specifications->manifest '("texlive" ;The TeX Live package, a comprehensive TeX system
+                            "texlive-biblatex" ;Complete reimplementation of bibliographic facilities for LaTeX
+                            "texlive-biber" ;A BibTeX replacement for users of BibLaTeX
+                            "texlive-tex-gyre" ;A collection of fonts based on the URW++ fonts
+                            "texlive-tex-gyre-math" ;Maths fonts to match tex-gyre text fonts
+                            ))
+
