26d4086e5ae491fa1eebdd3500feda81a26f3f40
Ayan Das <bvits@riseup.net>
2024-04-17 02:40:40 +0530

    feat: add new fonts and apply guix style

--- guix/manifests/fonts-manifest.scm
+++ guix/manifests/fonts-manifest.scm
@@ -1,16 +1,22 @@
+;; -*- mode: scheme; -*-
+;; fonts-manifest.scm
 (use-modules (gnu packages fonts))
 
-(specifications->manifest '(
-"font-gnu-unifont"
-"font-gnu-freefont"
-"font-google-noto"
-"font-google-noto-serif-cjk"
-"font-google-noto-sans-cjk"
-"font-google-noto"
-"font-liberation"
-"font-hack"
-"font-fira-go"
-"font-fira-mono"
-"font-fira-code"
-"font-fira-sans"
-))
+(specifications->manifest '("font-gnu-unifont" "font-gnu-freefont"
+                            "font-google-noto"
+                            "font-google-noto-serif-cjk"
+                            "font-google-noto-sans-cjk"
+                            "font-google-noto"
+                            "font-liberation"
+                            "font-hack"
+                            "font-fira-go"
+                            "font-fira-mono"
+                            "font-fira-code"
+                            "font-fira-sans"
+                            "font-tamzen"
+                            "font-terminus"
+                            "font-openmoji"
+                            "font-mathjax"
+                            "font-libertinus"
+                            "font-lato"
+                            "font-jetbrains-mono"))
