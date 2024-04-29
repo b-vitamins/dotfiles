652ff65cb39c17157aa408db01a8ae7457bb0836
Ayan Das <bvits@riseup.net>
2024-04-29 06:10:26 +0530

    feat: add guile-manifest.scm
    Packages:
    1) guile
    2) guile-lib
    3) guile-studio
    4) guile-sqlite3
    5) guile-json
    6) guile-goblins
    7) guile-fibers
    8) guile-dbi
    9) guile-openai
    10) guile-lens
    11) guile-xapian

---
+++ guix/manifests/guile-manifest.scm
@@ -0,0 +1,15 @@
+;; -*- mode: scheme; -*-
+;; guile-manifest.scm
+(use-modules (gnu packages guile)
+             (gnu packages guile-xyz))
+
+(specifications->manifest '("guile" "guile-lib"
+                            "guile-studio"
+                            "guile-sqlite3"
+                            "guile-json"
+                            "guile-goblins"
+                            "guile-fibers"
+                            "guile-dbi"
+                            "guile-openai"
+                            "guile-lens"
+                            "guile-xapian"))
