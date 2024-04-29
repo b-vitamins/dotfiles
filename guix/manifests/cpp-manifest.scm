3b93db4cba57914f03765621f07bce0763bcde83
Ayan Das <bvits@riseup.net>
2024-04-29 06:30:30 +0530

    bugfix: no package called kaldi-for-vosk, so remove it

--- guix/manifests/cpp-manifest.scm
+++ guix/manifests/cpp-manifest.scm
@@ -3,7 +3,6 @@
 (use-modules (gnu packages machine-learning))
 
 (specifications->manifest '("kaldi" ;Speech recognition toolkit
-                            "kaldi-for-vosk" ;Speech recognition toolkit
                             "kaldi-gstreamer-server" ;Real-time full-duplex speech recognition server
                             "llama-cpp" ;Port of Facebook's LLaMA model in C/C++
                             ))
