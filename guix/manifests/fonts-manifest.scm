052ad45383975e93e30519343ea55a567626c3a9
Ayan Das <bvits@riseup.net>
2024-04-30 23:50:08 +0530

    refactor: the Google Noto fonts will be installed system-wide
    They have been moved to the main config.scm file

--- guix/manifests/fonts-manifest.scm
+++ guix/manifests/fonts-manifest.scm
@@ -3,10 +3,6 @@
 (use-modules (gnu packages fonts))
 
 (specifications->manifest '("font-gnu-unifont" "font-gnu-freefont"
-                            "font-google-noto"
-                            "font-google-noto-serif-cjk"
-                            "font-google-noto-sans-cjk"
-                            "font-google-noto"
                             "font-liberation"
                             "font-hack"
                             "font-fira-go"
