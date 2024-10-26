5782aa09f49070692da82cabbf6c4ce90b33189f
Ayan Das <bvits@riseup.net>
2024-10-26 23:03:12 +0530

    emacs: Add Org mode config for Column View and Effort Estimates.

--- emacs/init.el
+++ emacs/init.el
@@ -814,6 +814,12 @@ FORMAT-STRING is the message to display, with optional ARGS for formatting."
             clock-total-time-cell-format "*%s*"
             duration-format '((special . h:mm)))
 
+  ;; Column View and Effort Estimates
+  (:option* columns-default-format
+            "%40ITEM(Task) %Effort(Estimate){:} %CLOCKSUM(Clocked)")
+  (:option* global-properties
+            '(("Effort_ALL" . "0 0:30 1:00 1:30 2:00 2:30 3:00 3:30 4:00 4:30 5:00 5:30 6:00 6:30 7:00 7:30 8:00 8:30 9:00 9:30 10:00 10:30 11:00 11:30 12:00 12:30 13:00 13:30 14:00 14:30 15:00 15:30 16:00 16:30 17:00 17:30 18:00 18:30 19:00 19:30 20:00 20:30 21:00 21:30 22:00 22:30 23:00 23:30 24:00")))
+
   ;; Logging and Archiving
   (:option* log-done 'time
             export-kill-after-export t)
