51148ed672043f67cb48958b054f7caf3f0b4d78
Ayan Das <bvits@riseup.net>
2024-12-17 08:49:48 +0530

    floki: Refactor to run as a VM.

--- guix/machines/floki.scm
+++ guix/machines/floki.scm
@@ -1,6 +1,18 @@
 ;; -*- mode: scheme; -*-
-(use-modules (gnu))
-(use-service-modules avahi cuirass networking ssh xorg)
+(use-modules (gnu)
+             (guix)
+             (srfi srfi-1))
+(use-service-modules avahi cuirass mcron networking ssh)
+(use-package-modules bootloaders
+                     certs
+                     fonts
+                     nvi
+                     package-management
+                     wget
+                     xorg)
+
+(define garbage-collector-job
+  #~(job "0 3 * * *" "guix gc -F 50G"))
 
 (operating-system
   (host-name "floki")
@@ -11,22 +23,31 @@
                                     #:options '("ctrl:nocaps"
                                                 "altwin:swap_alt_win")))
 
+  ;; Label for the GRUB boot menu.
+  (label (string-append "GNU Guix "
+                        (package-version guix)))
+
   ;; The list of user accounts ('root' is implicit).
-  (users (cons* (user-account
-                  (name "b")
-                  (comment "Ayan")
-                  (group "users")
-                  (home-directory "/home/b")
-                  (shell (file-append (specification->package "zsh")
-                                      "/bin/zsh"))
-                  (supplementary-groups '("wheel" "netdev" "audio" "video")))
-                %base-user-accounts))
+  (users (cons (user-account
+                 (name "guest")
+                 (comment "GNU Guix Live")
+                 (password "")
+                 (group "users")
+                 (supplementary-groups '("wheel" "netdev" "audio" "video")))
+               %base-user-accounts))
+
+  ;; Our /etc/sudoers file.  Since 'guest' initially has an empty password,
+  ;; allow for password-less sudo.
+  (sudoers-file (plain-file "sudoers" "root ALL=(ALL) ALL
+%wheel ALL=NOPASSWD: ALL
+"))
 
   ;; Packages installed system-wide.  Users can also install packages
   ;; under their own account: use 'guix search KEYWORD' to search
   ;; for packages and 'guix install PACKAGE' to install a package.
   (packages (append (list (specification->package "git")
                           (specification->package "wget")
+                          (specification->package "nvi")
                           (specification->package "curl")
                           (specification->package "emacs-no-x-toolkit")
                           (specification->package "htop")
@@ -44,7 +65,7 @@
                                                                (public-key
                                                                 "/etc/guix/signing-key.pub")
                                                                (server
-                                                                "10.0.0.3:5555")
+                                                                "10.0.2.2:5555")
                                                                (substitute-urls '
                                                                 ("https://ci.guix.gnu.org"
                                                                  "https://substitutes.myguix.bvits.in"))
@@ -55,6 +76,8 @@
                           (openssh-configuration (authorized-keys `(("b" ,(local-file
                                                                            "../../keys/ssh/helga.pub"))))
                                                  (password-authentication? #f)))
+                 (simple-service 'cron-jobs mcron-service-type
+                                 (list garbage-collector-job))
                  (service wpa-supplicant-service-type)
                  (service network-manager-service-type)
                  (service ntp-service-type)
@@ -65,30 +88,18 @@
            (modify-services %base-services
              (guix-service-type config =>
                                 (guix-configuration (inherit config)
-                                                    (generate-substitute-key?
-                                                                              #f)
                                                     (authorized-keys (append
                                                                       %default-authorized-guix-keys
                                                                       (list (local-file
                                                                              "../../keys/guix/helga.pub")))))))))
-  (bootloader (bootloader-configuration
-                (bootloader grub-efi-bootloader)
-                (targets (list "/boot/efi"))
-                (keyboard-layout keyboard-layout)))
-  (initrd-modules (append '("virtio_scsi") %base-initrd-modules))
-  (swap-devices (list (swap-space
-                        (target (uuid "22f7360c-5978-4c3e-954b-8fab6213dcde")))))
 
-  ;; The list of file systems that get "mounted".  The unique
-  ;; file system identifiers there ("UUIDs") can be obtained
-  ;; by running 'blkid' in a terminal.
-  (file-systems (cons* (file-system
-                         (mount-point "/boot/efi")
-                         (device (uuid "081A-297F"
-                                       'fat32))
-                         (type "vfat"))
-                       (file-system
-                         (mount-point "/")
-                         (device (uuid "ae3725bb-022c-4599-93b7-e229cb747a78"
-                                       'ext4))
-                         (type "ext4")) %base-file-systems)))
+  ;; Below we assume /dev/vda is the VM's hard disk.
+  ;; Adjust as needed.
+  (bootloader (bootloader-configuration
+                (bootloader grub-bootloader)
+                (targets '("/dev/vda"))
+                (terminal-outputs '(console))))
+  (file-systems (cons (file-system
+                        (mount-point "/")
+                        (device "/dev/vda2")
+                        (type "ext4")) %base-file-systems)))
