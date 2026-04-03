fd9d4b1c3730d45799cdc050913d771d5dd6851b
Ayan Das <bvits@riseup.net>
2026-04-03 20:37:26 +0530

    fix(guix): stop confirming ssh key use on mileva
    Switch the home OpenSSH configuration from AddKeysToAgent=confirm\nto AddKeysToAgent=yes on mileva so Guix home reconfigure does\nnot reintroduce per-use SSH agent confirmation prompts.

--- guix/machines/mileva.scm
+++ guix/machines/mileva.scm
@@ -422,7 +422,7 @@ allow-preset-passphrase")))
   StrictHostKeyChecking ask
   VerifyHostKeyDNS yes
   VisualHostKey yes"))))
-                                           (add-keys-to-agent "confirm")))
+                                           (add-keys-to-agent "yes")))
 
       ;; Desktop Home Services
       (service home-dbus-service-type)
